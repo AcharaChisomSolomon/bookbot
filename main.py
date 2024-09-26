612c6899ecfcff8b9a307193124e3fb54f83b5d6
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-09-26 21:04:58 +0100

    format the output neatly

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+FILE_PATH = "books/frankenstein.txt"
+
 def get_word_count(text):
     return len(text.split())
 
 def read_file(file_path):
     with open(file_path, "r") as file:
         return file.read()
+    
+def sort_dict_by_value(dictionary):
+    return dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
     
 def get_alphabet_count(text):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -14,12 +19,15 @@ def get_alphabet_count(text):
     return alphabet_count
 
 def main():
-    text = read_file("./books/frankenstein.txt")
+    text = read_file(FILE_PATH)
     word_count = get_word_count(text)
     alphabet_count = get_alphabet_count(text)
-    print(f"Word count: {word_count}")
-    print("Alphabet count:")
+    alphabet_count = sort_dict_by_value(alphabet_count)
+    print(f"--- Begin report of {FILE_PATH} ---")
+    print(f"{word_count} words found in the document.")
+    print()
     for letter, count in alphabet_count.items():
-        print(f"{letter}: {count}")
+        print(f"The '{letter}' character was found {count} times")
+    print(f"--- End report ---")
 
 main()
