b8fe13ca8e4a18ee0a80543feb40265b6e545b05
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-09-26 20:55:16 +0100

    add function to count each character

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+def get_word_count(text):
+    return len(text.split())
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+    
+def get_alphabet_count(text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet_count = {letter: 0 for letter in alphabet}
+    for letter in text:
+        if letter.lower() in alphabet:
+            alphabet_count[letter.lower()] += 1
+    return alphabet_count
+
 def main():
-    with open("./books/frankenstein.txt", "r") as file:
-        text = file.read()
-        print(text)
+    text = read_file("./books/frankenstein.txt")
+    word_count = get_word_count(text)
+    alphabet_count = get_alphabet_count(text)
+    print(f"Word count: {word_count}")
+    print("Alphabet count:")
+    for letter, count in alphabet_count.items():
+        print(f"{letter}: {count}")
 
 main()
